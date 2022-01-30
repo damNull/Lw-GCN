ee07afd43a8604fc7fa8d24a010273f8e42f459b
damNull <damNull@outlook.com>
2022-01-30 22:28:13 +0800

    fix bugs

--- eval.py
+++ eval.py
@@ -202,7 +202,6 @@ class Processor():
         output_device = self.arg.device[0] if type(self.arg.device) is list else self.arg.device
         self.output_device = output_device
         Model = import_class(self.arg.model)
-        shutil.copy2(inspect.getfile(Model), self.arg.work_dir)
         self.model = Model(**self.arg.model_args).cuda(output_device)
         self.loss = nn.CrossEntropyLoss().cuda(output_device)
 
---
+++ feeders/__init__.py
@@ -0,0 +1,4 @@
+from . import tools
+from . import feeder
+from . import feeder_squeeze
+from . import NWUCLA_feeder
